48f5a18a85122c2ba6ae6411acb9ebf2240693e0
3onyc <3onyc@x3tech.com>
2014-10-26 17:02:33 +0100

    Remove obsolete config variables

--- config/config.php
+++ config/config.php
@@ -1,7 +1,5 @@
 <?php
 return array(
-    'app'           => 'laravel',
-    'vendor'        => 'acme',
     'maintainer'    => 'Foo <foo@acme.tld>',
     'port'          => 8080,
     'uid'           => 1000,
