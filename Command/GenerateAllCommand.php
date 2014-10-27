c4bfbd5a2783efa1a9cbacf6f41e3056f48d8d68
3onyc <3onyc@x3tech.com>
2014-10-27 23:13:00 +0100

    Merge GenerateAllCommand

--- Command/GenerateAllCommand.php
+++ Command/GenerateAllCommand.php
@@ -23,16 +23,6 @@ class GenerateAllCommand extends Command
     protected $description = "Generate Dockerfile and fig.yml";
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
