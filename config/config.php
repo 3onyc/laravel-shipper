663b8ed31a9e87016c35f4ffd3097bd2220c2f5a
3onyc <3onyc@x3tech.com>
2014-11-01 17:20:58 +0100

    Make 'volumes' config consistent with the 'guest => host' format of arrays

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@ return array(
     'uid'           => 1000,
     'mount_volumes' => array('local'),
     'volumes'       => array(
-        '.:/var/www',
-        './app/storage/logs/hhvm:/var/log/hhvm',
-        './app/storage/logs/nginx:/var/log/nginx'
+        '/var/www' => '.',
+        '/var/log/hhvm' => './app/storage/logs/hhvm',
+        '/var/log/nginx' => './app/storage/logs/nginx'
     ),
     'test_cmd'      => 'vendor/bin/phpunit'
 );
