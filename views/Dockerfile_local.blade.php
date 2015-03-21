13dd70663ea67e20c10615db6fbedeb8d9cd8746
3onyc <3onyc@x3tech.com>
2015-03-21 21:28:44 +0100

    NGINX now uses template, update Dockerfile accordingly

--- views/Dockerfile_local.blade.php
+++ views/Dockerfile_local.blade.php
@@ -3,11 +3,11 @@ MAINTAINER {{ $maintainer }}
 
 WORKDIR /var/www
 
-ADD <?= str_replace(base_path() . '/', '', LARAVEL_SHIPPER_ROOT) ?>/resources/nginx-hhvm.conf /etc/nginx/nginx.conf
+ADD {{ str_replace(base_path() . '/', '', LARAVEL_SHIPPER_ROOT) }}/resources/nginx-hhvm.conf.tpl /etc/nginx/nginx.conf.tpl
 
 # Set user to 1000 so we can map it to logged in user
 RUN useradd -d /var/www -u {{ $uid }} www && \
-    sed -i 's/www-data/www/g' /etc/nginx/nginx.conf && \
+    sed -i 's/www-data/www/g' /etc/nginx/nginx.conf.tpl && \
     sed -i 's/www-data/www/g' /etc/service/hhvm/run && \
     chown -R www:www \
         /var/run/hhvm \
